7abd9afbcd6e7b7983845c950d377757cbebc194
erealt <82050971+erealt@users.noreply.github.com>
2021-11-09 12:11:02 +0100

    Update mapa.py

--- mapa.py
+++ mapa.py
@@ -1,23 +1,42 @@
 import os # This is is needed in the pyqgis console also
+
+import qgis
 from qgis.core import *
-from pathlib import Path
+from qgis.utils import *
 
-QgsApplication.setPrefixPath("C:\Program Files\QGIS 3.10", True)
+
+QgsApplication.setPrefixPath("/usr", True)
 qgs = QgsApplication([], False)
-
 
 qgs.initQgis()
 
-uri = "file:///C:/Users/realt/PycharmProjects/pythonProject/aire.csv?delimiter={}&xField={}&yField={}&crs={}".format(",", "x", "y","epsg:4326")
+#AÑADE CAPA CON LOS DATOS DE LOS 3 PUNTOS DEL CSV
+uri = "file:////home/estela/PycharmProjects/mapas/aire.csv?delimiter={}&xField={}&yField={}&crs={}".format(",", "x", "y","epsg:4326")
 vlayer = QgsVectorLayer(uri, "capa", "delimitedtext")
+
+
 QgsProject.instance().addMapLayer(vlayer)
 
 
-path = r'C:\Users\realt\OneDrive\Escritorio\aire\aire'
+#AÑADE MAPA BASE
+from qgis.core import QgsRasterLayer, QgsProject
 
 
-options = QgsVectorFileWriter.SaveVectorOptions()
-options.driverName = "ESRI Shapefile"
+def loadXYZ(url, name):
+   rasterLyr = QgsRasterLayer("type=xyz&url=" + url, name, "wms")
+   QgsProject.instance().addMapLayer(rasterLyr)
 
-QgsVectorFileWriter.writeAsVectorFormatV2(vlayer, path, QgsCoordinateTransformContext(), options)
 
+
+urlWithParams = 'https://tile.openstreetmap.org/%7Bz%7D/%7Bx%7D/%7By%7D.png&zmax=19&zmin=0&crs=EPSG3857'
+loadXYZ(urlWithParams, 'OpenStreetMap')
+
+
+
+#path = '/home/estela/aire/aire'
+
+
+#options = QgsVectorFileWriter.SaveVectorOptions()
+#options.driverName = "ESRI Shapefile"
+
+#QgsVectorFileWriter.writeAsVectorFormatV2(vlayer, path, QgsCoordinateTransformContext(), options)
