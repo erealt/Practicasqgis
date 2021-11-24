ab58765dd829a121b72b5ce50e44b1995b3a6700
erealt <82050971+erealt@users.noreply.github.com>
2021-11-24 11:05:10 +0100

    Update map.js

--- mapaconGrafica/map.js
+++ mapaconGrafica/map.js
@@ -1,14 +1,32 @@
 const tilesProvider ='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-var puntosAire; //añadimos los datos de nuestra capa propia
 
-var hoy = new Date();
+/*var hoy = new Date();
 var fecha = hoy.getDate() + '-' + ( hoy.getMonth() + 1 ) + '-' + hoy.getFullYear();
 var hora = hoy.getHours() + ':' + hoy.getMinutes() + ':' + hoy.getSeconds();
-var fechaYhora = fecha + ' ' + hora;
-let myMap = L.map('myMap').setView([38.99, -1.86],13)
+var fechaYhora = fecha + ' ' + hora;*/
 
-L.tileLayer(tilesProvider, {maxZoom: 18,}).addTo(myMap)
+let myMap = L.map('myMap').setView([38.99, -1.86],13) 
+// crea el mapa con una posicion determinada en este caso la de Albacete, el 13 es el zoom inicial
 
-$.getJSON('puntosAire.json',function(puntosAire){
-L.geoJson(puntosAire).addTo(myMap).bindPopup("<h1>Marcador</h1><p>Datos del día:</p>"+fechaYhora );
-})
+L.tileLayer(tilesProvider, {maxZoom: 18,}).addTo(myMap) 
+//agregamos al layer el mapa con openstream
+
+
+
+let geojson="puntosAire.json"
+
+fetch(
+geojson
+).then(
+    res=> res.json()
+
+).then(
+    data=>{
+       let geojsonlayer=L.geoJson(data,{
+           onEachFeature:function(features,layer){
+               layer.bindPopup(features.properties['idStation'])
+           }
+       }).addTo(myMap)
+    }
+)
+
