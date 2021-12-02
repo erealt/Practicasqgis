27c868bce084de4b53aee2b4e827b7cf599e2375
erealt <82050971+erealt@users.noreply.github.com>
2021-12-02 12:28:33 +0100

    Update map.js

--- mapaconGrafica/map.js
+++ mapaconGrafica/map.js
@@ -1,4 +1,3 @@
-
 const tilesProvider ='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
 var datosAire= //añadimos los datos de nuestra capa propia
 {
@@ -15,7 +14,20 @@ var datosAire= //añadimos los datos de nuestra capa propia
 let myMap = L.map('myMap').setView([38.99, -1.86],13)
 
 L.tileLayer(tilesProvider, {maxZoom: 18,}).addTo(myMap)
- L.geoJson(datosAire).addTo(myMap) ;
+L.geoJson(datosAire, {
+    onEachFeature:function(features,layer){
+        layer.bindPopup('<b>'+features.properties['idStation']+'</b>'+'</br>'
+        +'<button class="w3-button w3-teal w3-xlarge" onclick="w3_open()">Grafica</button>') ;
+    }
+}).addTo(myMap)
 
 
+function w3_open() {
+    document.getElementById("mySidebar").style.display = "block";
+    document.getElementById("mySidebar").style.width="50%"
+ }
+
+ function w3_close() {
+    document.getElementById("mySidebar").style.display = "none";
+ }
 
